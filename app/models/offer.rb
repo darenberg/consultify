05cc7b7d9b2a4c134e5beffdf5c62bc55d16309a
alienor darenberg <alienordarenberg@gmail.com>
2023-03-14 12:43:10 -0600

    added booking link to offer show

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,11 +1,8 @@
 class Offer < ApplicationRecord
   belongs_to :user, class_name: "User"
   has_many :bookings
-<<<<<<< Updated upstream
-=======
 
   validates :title, presence: true
   validates :category, presence: true
   validates :description, presence: true
->>>>>>> Stashed changes
 end
