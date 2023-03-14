6f9d810ba2e79737aa06418d4c55693263ed9cb4
alienor darenberg <alienordarenberg@gmail.com>
2023-03-14 11:44:24 -0600

    bookings controller 1st draft

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,38 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:update, :destroy]
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @offer = Offer.find(params[:offer_id])
+    @booking.offer = @offer
+    if @booking.save
+      redirect_to offer_booking_path
+    else
+      render @offer, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to offer_booking_path, status: :see_other
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:intitial_time, :end_time, :status, :offer_id, :student_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :dashboard, only: :show
 
   resources :offers do
-    resources :bookings, only: [:create, :update]
+    resources :bookings, only: [:show, :create, :update]
   end
 
   resources :bookings, only: :destroy do
