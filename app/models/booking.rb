ca2a1634371bf85bfda6d5498c99fcfbab50235e
alienor darenberg <alienordarenberg@gmail.com>
2023-03-14 12:34:01 -0600

    models validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :offer
   belongs_to :student, class_name: "User"
   has_one :teacher, through: :offer
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,11 @@
 class Offer < ApplicationRecord
   belongs_to :user, class_name: "User"
   has_many :bookings
+<<<<<<< Updated upstream
+=======
+
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+>>>>>>> Stashed changes
 end
