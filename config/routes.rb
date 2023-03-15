52323a1761e9075feb2029ec82fad8372b5c9d79
david betech <betech35@gmail.com>
2023-03-15 12:37:29 -0600

    teacher feature added completly

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :dashboard, only: :show
-
+  get '/teacher_offers', to: 'offers#offers_teacher'
   resources :offers do
-    resources :bookings, only: [:create, :update]
+    resources :bookings, only: [:show, :create, :update, :index]
   end
-
   resources :bookings, only: :destroy do
     member do
       patch :approve, :reject, :pending
