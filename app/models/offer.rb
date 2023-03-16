3a812be52f1cdb22e317f6caf61d62100c3426e3
alienor darenberg <alienordarenberg@gmail.com>
2023-03-16 16:29:40 -0600

    push

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,22 +1,13 @@
 class Offer < ApplicationRecord
-<<<<<<< Updated upstream
-
-=======
-  include PgSearch::Model
->>>>>>> Stashed changes
   belongs_to :user, class_name: "User"
   has_many :bookings
 
   validates :title, presence: true
   validates :category, presence: true
   validates :description, presence: true
-<<<<<<< Updated upstream
-  validates :price,  numericality: { only_decimal: true }
-=======
   validates :price, presence: true
   validates :price, numericality: { only_decimal: true }
 
->>>>>>> Stashed changes
   include PgSearch::Model
   pg_search_scope :search_by_title_and_description_and_category,
   against: [ :title, :category, :description ],
